1c546af83b5aa3dd12706bc7dc342f57d72c93be
cody <cmchaff14@gmail.com>
2021-10-15 21:15:33 -0500

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+def greet_programmer
+  puts  "Hello, programmer!"
+end
+
+@name = "Naureen"
+def greet(name)
+   puts "Hello, #{name}!"
+end
+
+
+def greet_with_default(name ="Naureen" )
+    puts "Hello, #{name}!"
+    puts "Hello, programmer!"
+end
+
+def add(num1, num2)
+    num1 + num2
+end
+
+def halve(num)
+    if num.class == String
+        return nil
+    end
+    num / 2
+end
